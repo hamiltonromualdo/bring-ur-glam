1605f989a3e20be1e4a5ef8a74da91ca355626ed
hdpoliveira <hdpoliveira@gmail.com>
2021-11-13 21:08:04 -0300

    Reorganize playground directory and move to bring_ur_glam

---
+++ bring_ur_glam/src/Objects/HeartAmmo.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+export var DIRECTION = false
+
+const SPEED = 150
+var velocity = Vector2()
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	velocity.x = SPEED * delta
+	if not DIRECTION:
+		velocity.x *= -1
+	translate(velocity)
+
+func set_fireball_direction(dir):
+	DIRECTION = dir
+	$Sprite.flip_h = !dir
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+func _on_HeartAmmo_body_entered(body):
+	if "Enemy" in body.name:
+		body.hurt()
+	queue_free()
