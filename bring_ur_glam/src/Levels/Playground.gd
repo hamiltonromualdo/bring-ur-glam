b4c878cd0bb93720049ba8bd140d62842a4d9f05
hdpoliveira <hdpoliveira@gmail.com>
2021-11-13 23:27:16 -0300

    Automatic changes done by Godot

--- bring_ur_glam/src/Levels/Playground.gd
+++ bring_ur_glam/src/Levels/Playground.gd
@@ -3,10 +3,10 @@ extends Node2D
 const FIREBALL = preload("res://src/Objects/HeartAmmo.tscn")
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_shoot"):
-		var playerPosition = $Player.DIRECTION
+    if Input.is_action_just_pressed("ui_shoot"):
+        var playerPosition = $Player.DIRECTION
 
-		var fireball = FIREBALL.instance()
-		fireball.set_fireball_direction(playerPosition)
-		get_parent().add_child(fireball)
-		fireball.position = $Player/Position2D.global_position
+        var fireball = FIREBALL.instance()
+        fireball.set_fireball_direction(playerPosition)
+        get_parent().add_child(fireball)
+        fireball.position = $Player/Position2D.global_position
