319730be1f95775a275b5cce693a0c150d486b71
hdpoliveira <hdpoliveira@gmail.com>
2021-11-14 19:57:34 -0300

    Convert tabs to spaces in HeartAmmo.gd

--- bring_ur_glam/src/Objects/HeartAmmo.gd
+++ bring_ur_glam/src/Objects/HeartAmmo.gd
@@ -6,22 +6,22 @@ const SPEED = 150
 var velocity = Vector2()
 
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 func _physics_process(delta):
-	velocity.x = SPEED * delta
-	if not DIRECTION:
-		velocity.x *= -1
-	translate(velocity)
+    velocity.x = SPEED * delta
+    if not DIRECTION:
+        velocity.x *= -1
+    translate(velocity)
 
 func set_fireball_direction(dir):
-	DIRECTION = dir
-	$Sprite.flip_h = !dir
+    DIRECTION = dir
+    $Sprite.flip_h = !dir
 
 func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+    queue_free()
 
 func _on_HeartAmmo_body_entered(body):
-	if "Enemy" in body.name:
-		body.hurt()
-	queue_free()
+    if "Enemy" in body.name:
+        body.hurt()
+    queue_free()
