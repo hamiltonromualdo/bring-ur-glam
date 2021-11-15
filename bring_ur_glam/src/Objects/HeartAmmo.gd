76a2f2a326539723f3f5e69148aefd608c6aad33
hdpoliveira <hdpoliveira@gmail.com>
2021-11-14 21:04:11 -0300

    Reorder HeartAmmo function to have callbacks first

--- bring_ur_glam/src/Objects/HeartAmmo.gd
+++ bring_ur_glam/src/Objects/HeartAmmo.gd
@@ -5,23 +5,24 @@ export var DIRECTION = false
 const SPEED = 150
 var velocity = Vector2()
 
-func _ready():
-    pass # Replace with function body.
 
-func _physics_process(delta):
+func _on_HeartAmmo_body_entered(body) -> void:
+    if "Enemy" in body.name or "Player" in body.name:
+        body.hurt()
+    queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+    queue_free()
+
+
+func _physics_process(delta: float) -> void:
     velocity.x = SPEED * delta
     if not DIRECTION:
         velocity.x *= -1
     translate(velocity)
 
-func set_fireball_direction(dir):
+
+func set_fireball_direction(dir: bool) -> void:
     DIRECTION = dir
     $Sprite.flip_h = !dir
-
-func _on_VisibilityNotifier2D_screen_exited():
-    queue_free()
-
-func _on_HeartAmmo_body_entered(body):
-    if "Enemy" in body.name or "Player" in body.name:
-        body.hurt()
-    queue_free()
