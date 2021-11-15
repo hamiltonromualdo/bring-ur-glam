12f8bfb7d506239066b3cfcbe88bf55e5627a0a7
Thamara Andrade <tkcandrade@gmail.com>
2021-11-15 01:04:07 -0300

    Moving customization code to PlayerImage

--- bring_ur_glam/src/Actors/PlayerImages.gd
+++ bring_ur_glam/src/Actors/PlayerImages.gd
@@ -1,5 +1,55 @@
 extends Node2D
 
+onready var hairSprite = $CompositionalSprites/Hair
+onready var dressSprite = $CompositionalSprites/Dress
+onready var shoesSprite = $CompositionalSprites/Shoes
+onready var gunSprite = $CompositionalSprites/Gun
+onready var skinSprite = $CompositionalSprites/Skin
+
+var save_path = "user://player.dat"
+
+var hairIndex = 0
+var hair_spritesheet = {
+    0: preload("res://assets/player/hair_1.png"),
+    1: preload("res://assets/player/hair_2.png"),
+    2: preload("res://assets/player/hair_3.png"),
+    3: preload("res://assets/player/hair_4.png"),
+}
+var dress_spritesheet = preload("res://assets/player/dress_1.png")
+var shoes_spritesheet = preload("res://assets/player/shoes_1.png")
+var gun_spritesheet = preload("res://assets/player/gun_1.png")
+var skin_spritesheet = preload("res://assets/player/skin_1.png")
+
+func set_hair_color(color: Color) -> void:
+    hairSprite.modulate = color
+
+
+func set_dress_color(color: Color) -> void:
+    dressSprite.modulate = color
+
+
+func set_shoes_color(color: Color) -> void:
+    shoesSprite.modulate = color
+
+
+func set_gun_color(color: Color) -> void:
+    gunSprite.modulate = color
+
+
+func set_skin_color(color: Color) -> void:
+    skinSprite.modulate = color
+
+
+func set_hair_style(index: int) -> void:
+    hairIndex = index
+    hairSprite.texture = hair_spritesheet[index]
+
+
+func change_hair(incr: int) -> void:
+    var index = posmod(hairIndex + incr, len(hair_spritesheet))
+    set_hair_style(index)
+
+
 func flip_h(value: bool) -> void:
     for sprite in $CompositionalSprites.get_children():
         sprite.flip_h = value
@@ -9,12 +59,32 @@ func flip_v(value: bool) -> void:
     for sprite in $CompositionalSprites.get_children():
         sprite.flip_v = value
 
-var save_path = "user://player.dat"
-func load_default() -> void:
+func save_data() -> void:
+    var player_info = {
+        "hair_style": hairIndex,
+        "hair_color": hairSprite.modulate,
+        "dress_color": dressSprite.modulate,
+        "shoes_color": shoesSprite.modulate,
+        "gun_color": gunSprite.modulate,
+        "skin_color": skinSprite.modulate,
+    }
+    var file = File.new()
+    var error = file.open(save_path, File.WRITE)
+    if error == OK:
+        file.store_var(player_info)
+        file.close()
+
+func load_data() -> bool:
     var file = File.new()
     var error = file.open(save_path, File.READ)
     if error == OK:
         var player_info = file.get_var()
-        $CompositionalSprites/Hair.modulate = player_info["hair_color"]
+        set_hair_style(player_info["hair_style"])
+        set_hair_color(player_info["hair_color"])
+        set_dress_color(player_info["dress_color"])
+        set_shoes_color(player_info["shoes_color"])
+        set_gun_color(player_info["gun_color"])
+        set_skin_color(player_info["skin_color"])
         file.close()
-    
+    return error == OK
+
--- bring_ur_glam/src/UserInterface/CustomizePlayer.gd
+++ bring_ur_glam/src/UserInterface/CustomizePlayer.gd
@@ -1,67 +1,27 @@
 extends Node2D
 
-onready var hairSprite = $PlayerImages/CompositionalSprites/Hair
-onready var dressSprite = $PlayerImages/CompositionalSprites/Dress
-onready var shoesSprite = $PlayerImages/CompositionalSprites/Shoes
-onready var gunSprite = $PlayerImages/CompositionalSprites/Gun
-onready var skinSprite = $PlayerImages/CompositionalSprites/Skin
-
 onready var hairColor = $VBoxContainer/HairColor
 onready var skinColor = $VBoxContainer/SkinColor
 onready var gunColor = $VBoxContainer/GunColor
 onready var dressColor = $VBoxContainer/DressColor
 onready var shoesColor = $VBoxContainer/ShoesColor
 
+onready var player = $PlayerImages
+
 var save_path = "user://player.dat"
 
-var hairIndex = 0
-var hair_spritesheet = {
-    0: preload("res://assets/player/hair_1.png"),
-    1: preload("res://assets/player/hair_2.png"),
-    2: preload("res://assets/player/hair_3.png"),
-    3: preload("res://assets/player/hair_4.png"),
-}
-var dress_spritesheet = preload("res://assets/player/dress_1.png")
-var shoes_spritesheet = preload("res://assets/player/shoes_1.png")
-var gun_spritesheet = preload("res://assets/player/gun_1.png")
-var skin_spritesheet = preload("res://assets/player/skin_1.png")
-
-func set_hair_color(color: Color) -> void:
-    hairSprite.modulate = color
-
-
-func set_dress_color(color: Color) -> void:
-    dressSprite.modulate = color
-
-
-func set_shoes_color(color: Color) -> void:
-    shoesSprite.modulate = color
-
-
-func set_gun_color(color: Color) -> void:
-    gunSprite.modulate = color
-
-
-func set_skin_color(color: Color) -> void:
-    skinSprite.modulate = color
-
-
-func change_hair(incr: int) -> void:
-    hairIndex = posmod(hairIndex + incr, len(hair_spritesheet))
-    hairSprite.texture = hair_spritesheet[hairIndex]
-
-
 func randomize_char() -> void:
-    change_hair(randi())
-    set_hair_color(hairColor.get_rand_color())
-    set_dress_color(dressColor.get_rand_color())
-    set_shoes_color(shoesColor.get_rand_color())
-    set_gun_color(gunColor.get_rand_color())
-    set_skin_color(skinColor.get_rand_color())
-
+    player.change_hair(randi())
+    player.set_hair_color(hairColor.get_rand_color())
+    player.set_dress_color(dressColor.get_rand_color())
+    player.set_shoes_color(shoesColor.get_rand_color())
+    player.set_gun_color(gunColor.get_rand_color())
+    player.set_skin_color(skinColor.get_rand_color())
 
 func _ready() -> void:
-    randomize_char()
+    var loaded = player.load_data()
+    if not loaded:
+        randomize_char()
 
 
 func _on_Rand_pressed() -> void:
@@ -69,45 +29,32 @@ func _on_Rand_pressed() -> void:
 
 
 func _on_HairChangeLeft_pressed() -> void:
-    change_hair(-1)
+    player.change_hair(-1)
 
 
 func _on_HairChangeRight_pressed() -> void:
-    change_hair(1)
+    player.change_hair(1)
 
 
 func _on_SkinTone_color_changed(color) -> void:
-    set_skin_color(color)
+    player.set_skin_color(color)
 
 
 func _on_HairColor_color_changed(color) -> void:
-    set_hair_color(color)
+    player.set_hair_color(color)
 
 
 func _on_GunColorSelector_color_changed(color) -> void:
-    set_gun_color(color)
+    player.set_gun_color(color)
 
 
 func _on_DressColor_color_changed(color) -> void:
-    set_dress_color(color)
+    player.set_dress_color(color)
 
 
 func _on_ShoesColor_color_changed(color) -> void:
-    set_shoes_color(color)
+    player.set_shoes_color(color)
 
 
 func _on_Save_pressed() -> void:
-    var player_info = {
-        "hair_style": hairIndex,
-        "hair_color": hairSprite.modulate,
-        "dress_color": dressSprite.modulate,
-        "shoes_color": shoesSprite.modulate,
-        "gun_color": gunSprite.modulate,
-        "skin_color": skinSprite.modulate,
-    }
-    var file = File.new()
-    var error = file.open(save_path, File.WRITE)
-    if error == OK:
-        file.store_var(player_info)
-        file.close()
-
+    player.save_data()
