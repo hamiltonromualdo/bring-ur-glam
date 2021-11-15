fadc10204fc37ee430828199f711ace35675a341
Thamara Andrade <tkcandrade@gmail.com>
2021-11-15 01:25:43 -0300

    Back to the Customization Scene from the pause screen

--- bring_ur_glam/src/UserInterface/PauseScreen.gd
+++ bring_ur_glam/src/UserInterface/PauseScreen.gd
@@ -27,3 +27,8 @@ func _on_Resume_pressed():
 
 func _on_Exit_pressed():
     get_tree().quit()
+
+
+func _on_Restart_pressed() -> void:
+    toggle_visibility()
+    get_tree().change_scene("res://src/UserInterface/CustomizePlayer.tscn")
