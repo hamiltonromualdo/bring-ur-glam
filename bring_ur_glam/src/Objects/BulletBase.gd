03fc58ada737185a62c3484cf4019709fcdac3ff
Thamara Andrade <tkcandrade@gmail.com>
2021-11-15 21:01:21 -0300

    Using different bullet for the enemy

---
+++ bring_ur_glam/src/Objects/BulletBase.gd
@@ -0,0 +1,29 @@
+extends Area2D
+class_name BulletBase
+
+export var DIRECTION = false
+
+const SPEED = 150
+var velocity = Vector2()
+
+
+func _on_Bullet_body_entered(body) -> void:
+    if body is Enemy or body is Player:
+        body.hurt()
+    queue_free()
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+    queue_free()
+
+
+func _physics_process(delta: float) -> void:
+    velocity.x = SPEED * delta
+    if not DIRECTION:
+        velocity.x *= -1
+    translate(velocity)
+
+
+func set_fireball_direction(dir: bool) -> void:
+    DIRECTION = dir
+    $Sprite.flip_h = !dir
