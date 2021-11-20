46735b108788e3d0768633d17d6e19436af1edc5
Thamara Andrade <tkcandrade@gmail.com>
2021-11-20 20:05:49 -0300

    Removing debug from code

--- bring_ur_glam/src/UserInterface/HealthBar.gd
+++ bring_ur_glam/src/UserInterface/HealthBar.gd
@@ -12,21 +12,16 @@ func set_values(min_value, max_value, value):
 func set_hp(value_to_set):
     $ProgressBar.value = value_to_set
     $ProgressBar/Timer.start()
-    print("Start timer")
 
 func _ready():
     set_process(false)
 
 func _process(delta):
     $ProgressBar/ProgressBar2.value = lerp($ProgressBar/ProgressBar2.value, $ProgressBar.value, 0.1)
-    print($ProgressBar/ProgressBar2.value)
-    print($ProgressBar.value)
-    print("")
     if ($ProgressBar/ProgressBar2.value - $ProgressBar.value <= 5):
         $ProgressBar/ProgressBar2.value = $ProgressBar.value
         set_process(false)
         $ProgressBar/Timer.stop()
 
 func _on_Timer_timeout():
-    print("Timer finished")
     set_process(true)
