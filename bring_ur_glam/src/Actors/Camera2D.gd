3170c9704c91e50616a6ea4a070835bf9f7468f1
Thamara Andrade <tkcandrade@gmail.com>
2021-11-20 03:09:07 -0300

    Fixing camera movement

--- bring_ur_glam/src/Actors/Camera2D.gd
+++ bring_ur_glam/src/Actors/Camera2D.gd
@@ -11,17 +11,7 @@ var facing = 0
 
 
 func _process(delta: float) -> void:
-    _check_facing()
     prev_camera_pos = get_camera_position()
-
-
-func _check_facing() -> void:
-    var new_facing = sign(get_camera_position().x - prev_camera_pos.x)
-    if new_facing != 0 && facing != new_facing:
-        facing = new_facing
-        var target_offset = get_viewport_rect().size.x * facing * LOOK_AHEAD_FACTOR
-        tween.interpolate_property(self, "position:x", position.x, target_offset, SHIFT_DURATION, SHIFT_TRANS, SHIFT_EASE)
-        tween.start()
 
 
 func _on_Player_grounded_updated(is_grounded) -> void:
