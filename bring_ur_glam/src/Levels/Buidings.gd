affda34cf05afd14942e47eb787a6a5e635f95bb
Thamara Andrade <tkcandrade@gmail.com>
2021-11-22 00:55:15 -0300

    Adding auto generation of buildings

---
+++ bring_ur_glam/src/Levels/Buidings.gd
@@ -0,0 +1,125 @@
+tool
+extends TileMap
+
+enum TILE_COLORS {NONE = -1, BROWN = 0, PURPLE = 1, RED = 2, GREY = 3, GREEN = 4}
+const TILE_COLORS_VEC = [TILE_COLORS.BROWN, TILE_COLORS.PURPLE, TILE_COLORS.RED, TILE_COLORS.GREY, TILE_COLORS.GREEN]
+const GENERATION_TILES = [5, 6, 7, 8, 9]
+const BROWN_BOTTOM_DOORS = [22, 59, 110, 111, 113]
+const GREY_BOTTOM_DOORS = [40, 112, 114, 115, 81]
+
+const PARTS = {
+    TILE_COLORS.BROWN: {
+        "Window": [[13, 12], [14, 12], [16, 15]],
+        "DoorDown": BROWN_BOTTOM_DOORS,
+        "DoorUp": [19, 20, 21],
+        "Roof": [11]
+    },
+    TILE_COLORS.PURPLE: {
+        "Window": [[32, 34], [33, 34]],
+        "DoorDown": GREY_BOTTOM_DOORS,
+        "DoorUp": [37, 38, 39],
+        "Roof": [31]
+    },
+    TILE_COLORS.RED: {
+        "Window": [[52, 53], [54, 55]],
+        "DoorDown": BROWN_BOTTOM_DOORS,
+        "DoorUp": [56, 57, 58],
+        "Roof": [51]
+    },
+    TILE_COLORS.GREY: {
+        "Window": [[72, 77], [76, 77], [73, 77], [74, 77], [75, 77]],
+        "DoorDown": GREY_BOTTOM_DOORS,
+        "DoorUp": [78, 79, 80],
+        "Roof": [71]
+    },
+    TILE_COLORS.GREEN: {
+        "Window": [[92, 93], [94, 96], [95, 96], [98, 99], [97, 99]],
+        "DoorDown": GREY_BOTTOM_DOORS,
+        "DoorUp": [100, 101, 102],
+        "Roof": [91]
+    }
+}
+
+func _get_generation_tile_color(tile: int):
+    # Generation tiles are from range [5, 9], so to get the color from the vector
+    # we need to subtract 5
+    return TILE_COLORS_VEC[tile - 5] if tile in GENERATION_TILES else TILE_COLORS.NONE
+
+
+func _get_tile_color(tile: int):
+    return TILE_COLORS_VEC[tile] if tile in TILE_COLORS_VEC else TILE_COLORS.NONE
+
+
+func _is_marker_tile(tile, color):
+    return _get_tile_color(tile) == color
+
+
+func _get_rand_from_vec(vec: Array):
+    return vec[randi() % len(vec)]
+
+
+func get_rand_window(color):
+    return _get_rand_from_vec(PARTS[color]["Window"])
+
+
+func get_rand_door(color):
+    return [_get_rand_from_vec(PARTS[color]["DoorDown"]), _get_rand_from_vec(PARTS[color]["DoorUp"])]
+
+
+func get_roof(color):
+    return _get_rand_from_vec(PARTS[color]["Roof"])
+
+
+func set_cell(x: int, y: int, 
+              tile: int,
+              flip_x: bool = false, flip_y: bool = false, 
+              transpose: bool = false, 
+              autotile_coord: Vector2 = Vector2( 0, 0 )) -> void:
+    var generation_tile_color = _get_generation_tile_color(tile)
+    if generation_tile_color != TILE_COLORS.NONE:
+        var color = generation_tile_color
+        # The generation tile should be placed on the left lower corner
+        # From there, we find the bounds of the building to be created
+        var x_bound = x + 1
+        var y_bound = y - 1
+        while _is_marker_tile(get_cell(x_bound, y), color):
+            x_bound += 1
+        while _is_marker_tile(get_cell(x, y_bound), color):
+            y_bound -= 1
+        var max_y = y
+        var x_tiles = x_bound - x
+        var y_tiles = y - y_bound
+        
+        if x_tiles <= 1 || y_tiles <= 1:
+            return
+        
+        # Windows
+        for x_pos in range(x, x_bound, 1):
+            for y_pos in range(y, y_bound, -2):
+                var tile_pair = get_rand_window(color)
+                .set_cellv(Vector2(x_pos, y_pos), tile_pair[0])
+                .set_cellv(Vector2(x_pos, y_pos-1), tile_pair[1])
+                max_y = max_y if y_pos-1 > max_y else y_pos-1
+
+        # Door
+        if (x_tiles % 2) == 0:
+            var x_pos = x + x_tiles/2 -1
+            var tile_pair = get_rand_door(color)
+            .set_cellv(Vector2(x_pos, y), tile_pair[0])
+            .set_cellv(Vector2(x_pos, y-1), tile_pair[1])
+            # Flip for on x axis
+            .set_cellv(Vector2(x_pos + 1, y), tile_pair[0], true)
+            .set_cellv(Vector2(x_pos + 1, y-1), tile_pair[1], true)
+        else:
+            # One door
+            var x_pos = x + x_tiles/2
+            var tile_pair = get_rand_door(color)
+            .set_cellv(Vector2(x_pos, y), tile_pair[0])
+            .set_cellv(Vector2(x_pos, y-1), tile_pair[1])
+#
+        # Roof
+        var y_pos = max_y - 1
+        for x_pos in range(x, x_bound, 1):
+            .set_cellv(Vector2(x_pos, y_pos), get_roof(color))
+    else: 
+        .set_cellv(Vector2(x, y), tile, flip_x, flip_y, transpose)
