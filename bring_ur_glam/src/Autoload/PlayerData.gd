145904aa2ec26f7bc62f4317cfb26e1e965a7c68
Thamara Andrade <tkcandrade@gmail.com>
2021-11-30 02:55:49 -0300

    Storing and loading audio settings

--- bring_ur_glam/src/Autoload/PlayerData.gd
+++ bring_ur_glam/src/Autoload/PlayerData.gd
@@ -10,6 +10,27 @@ var score: = 0 setget set_score
 var protected: = false setget set_protected
 
 
+func _ready() -> void:
+    var audio_file_path = "user://audio_settings.dat"
+    var file = File.new()
+    var error = file.open(audio_file_path, File.READ)
+    if error == OK:
+        var audio_info = file.get_var()
+        AudioServer.set_bus_volume_db(0, audio_info["master"])
+        AudioServer.set_bus_volume_db(1, audio_info["music"])
+        AudioServer.set_bus_volume_db(2, audio_info["effects"])
+        file.close()
+    else:
+        var audio_info = {
+            "master": AudioServer.get_bus_volume_db(0),
+            "music": AudioServer.get_bus_volume_db(1),
+            "effects": AudioServer.get_bus_volume_db(2),
+        }
+        file.store_var(audio_info)
+        file.close()
+        
+
+
 func reset() -> void:
     hp = total_hp
     score = 0
--- bring_ur_glam/src/UserInterface/SoundControl.gd
+++ bring_ur_glam/src/UserInterface/SoundControl.gd
@@ -12,6 +12,30 @@ onready var master_slider = $Control/MarginContainer/VBoxContainer/MainSlider
 onready var music_slider = $Control/MarginContainer/VBoxContainer/MusicSlider
 onready var effects_slider = $Control/MarginContainer/VBoxContainer/EffectsSlider
 
+
+var is_dirty = false
+
+
+func set_is_dirty() -> void:
+    if $Timer.is_stopped():
+        $Timer.start()
+    is_dirty = true
+
+
+func save_info() -> void:
+    print('save info')
+    var audio_file_path = "user://audio_settings.dat"
+    var file = File.new()
+    var error = file.open(audio_file_path, File.WRITE)
+    if error == OK:
+        var audio_info = {
+            "master": AudioServer.get_bus_volume_db(master_bus_index),
+            "music": AudioServer.get_bus_volume_db(music_bus_index),
+            "effects": AudioServer.get_bus_volume_db(effects_bus_index),
+        }
+        file.store_var(audio_info)
+        file.close()
+
 func _ready() -> void:
     master_slider.value = db2linear(AudioServer.get_bus_volume_db(master_bus_index))
     music_slider.value = db2linear(AudioServer.get_bus_volume_db(music_bus_index))
@@ -19,11 +43,20 @@ func _ready() -> void:
 
 func _on_MainSlider_value_changed(value: float) -> void:
     AudioServer.set_bus_volume_db(master_bus_index, linear2db(value))
+    set_is_dirty()
 
 
 func _on_MusicSlider_value_changed(value: float) -> void:
     AudioServer.set_bus_volume_db(music_bus_index, linear2db(value))
+    set_is_dirty()
 
 
 func _on_EffectsSlider_value_changed(value: float) -> void:
     AudioServer.set_bus_volume_db(effects_bus_index, linear2db(value))
+    set_is_dirty()
+
+
+func _on_Timer_timeout() -> void:
+    if is_dirty:
+        save_info()
+        is_dirty = false
